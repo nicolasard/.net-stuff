1d87b8395afe6c752faccd0e025740a690586aec
Nico-pcCasa <nicolas.ard@gmail.com>
2017-04-09 09:37:34 -0300

    Added MultiThreadLibs project

---
+++ MultiThreadLibs/MultiThreadProcessor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication2
+{
+    class MultiThreadProcessor
+    {
+        public void runTest()
+        {
+            // Example using Parallel at .net
+            String[] files = { "HELLO", "WORLD" };
+            String finalstring = "";
+
+            Parallel.ForEach(files, (currentFile) =>
+            {
+
+                if (currentFile.Equals("HELLO"))
+                {
+                    Thread.Sleep(2000);
+                    finalstring = finalstring + " " + currentFile;
+                }
+
+                if (currentFile.Equals("WORLD"))
+                {
+                    Thread.Sleep(1000);
+                    finalstring = finalstring + " " + currentFile;
+                }
+
+                Console.WriteLine("Processing {0} on thread {1}", currentFile, Thread.CurrentThread.ManagedThreadId);
+            });
+
+            Console.WriteLine("FinalString:" + finalstring);
+            Console.WriteLine("Processing complete. ");
+        }
+    }
+}
